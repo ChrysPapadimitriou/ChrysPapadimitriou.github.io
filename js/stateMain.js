bc6786922ab4c7c8ceac08c8dca418c9d270e7ae
ChrysPapadimitriou <chryspapacy@gmail.com>
2022-04-01 13:24:36 +0100

    v3

--- js/stateMain.js
+++ js/stateMain.js
@@ -1,11 +1,20 @@
 var StateMain = {
 
     preload: function () {
-
+        this.preload.image("background","assets/background.png");
     },
 
     create: function () {
         console.log("Ready!");
+        this.background = this.add.image(0,0,"background");
+
+        //set grid lines to locate positions
+        this.aGrid=new AlignGrid({scene:this,rows:11,cols:11});
+        this.aGrid.showNumbers();
+
+        //set background to gridline position 60
+        this.aGrid.PlaceAtIndex(60,this.background);
+        Align.scaleToGameW(this.background,.2);
     },
 
     update: function () {
