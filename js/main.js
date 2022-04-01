78fc6f7df606a567b70b9de10f32cbef832b37d0
ChrysPapadimitriou <chryspapacy@gmail.com>
2022-04-01 13:20:19 +0100

    v2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-var isMobile = navigator.userAgent.indexOf("Mobile");
-if (isMobile == -1) {
-    isMobile = navigator.userAgent.indexOf("Tablet");
+var game;
+window.onload = function()
+{
+	 var isMobile=navigator.userAgent.indexOf("Mobile");
+
+   if (isMobile==-1)
+    {
+        game=new Phaser.Game(480,640,Phaser.AUTO,"ph_game");
+    }
+    else
+    {       
+      game=new Phaser.Game(window.innerWidth,window.innerHeight,Phaser.AUTO,"ph_game");  
+      console.log("Mobile");
+    }
+
+    game.state.add("StateMain",StateMain);
+    game.state.start("StateMain");
 }
-if (isMobile == -1) {
-    var config = {
-        type: Phaser.AUTO,
-        width: 480,
-        height: 640,
-        parent: 'phaser-game',
-        scene: [SceneMain]
-    };
-} else {
-    var config = {
-        type: Phaser.AUTO,
-        width: window.innerWidth,
-        height: window.innerHeight,
-        parent: 'phaser-game',
-        scene: [SceneMain]
-    };
-}
-var game = new Phaser.Game(config);
