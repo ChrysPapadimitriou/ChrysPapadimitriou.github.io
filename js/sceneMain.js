2af43f7e1a6605bdc602371866e1c5e2658a8e6b
ChrysPapadimitriou <chryspapacy@gmail.com>
2022-04-01 14:09:16 +0100

    add background

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -4,10 +4,19 @@ class SceneMain extends Phaser.Scene {
     }
     preload()
     {
-    	
+        this.preload.image("background","assets/background.png");
     }
     create() {
         console.log("Ready!");
+        this.background = this.add.image(0,0,"background");
+
+        //set grid lines to locate positions
+        this.aGrid=new AlignGrid({scene:this,rows:11,cols:11});
+        this.aGrid.showNumbers();
+
+        //set background to gridline position 60
+        this.aGrid.PlaceAtIndex(60,this.background);
+        Align.scaleToGameW(this.background,.2);
     }
     update() {}
 }
