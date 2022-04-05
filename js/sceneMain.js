8aab6c22d3f306bd33e33f23d56fa215232e180e
Chryscy <chryspapacy@gmail.com>
2022-04-05 13:55:20 +0100

    v2

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -13,12 +13,12 @@ class SceneMain extends Phaser.Scene {
         bg.scaleX = bg.scaleY;
         bg.y= game.config.height/2;
         bg.x= game.config.width/2;
-        //bg.x = bg.displayWidth*.5;
+        bg.x = bg.displayWidth*.5;
         //set grid lines to locate positions
         this.blockGrid =new AlignGrid({
             scene:this,
-            rows:22,
-            cols:22,
+            rows:33,
+            cols:33,
             height:bg.displayHeight,
             width:bg.displayWidth
         });
