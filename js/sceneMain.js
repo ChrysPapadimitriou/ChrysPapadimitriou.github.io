e1e10d0d078faeeeb7edd9542f88d912ce46d09f
Chryscy <chryspapacy@gmail.com>
2022-04-11 14:30:48 +0100

    v1

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -46,8 +46,8 @@ class SceneMain extends Phaser.Scene {
 
         this.blockGrid.placeAtIndex(76,this.Welcome);
         this.blockGrid.placeAtIndex(208,this.Flag);
-        this.blockGrid.placeAtIndex(52,this.Left);
-        this.blockGrid.placeAtIndex(56,this.Right);
+        this.blockGrid.placeAtIndex(448,this.Left);
+        this.blockGrid.placeAtIndex(452,this.Right);
         this.blockGrid.placeAtIndex(208.65,this.Logo);
 
         this.bird = this.add.sprite(0,0,"bird");
@@ -56,15 +56,15 @@ class SceneMain extends Phaser.Scene {
 
         this.LeftHold = this.add.sprite(0,0,"LeftHold");
         Align.scaleToGameW(this.LeftHold,0.04);
-        this.blockGrid.placeAtIndex(52,this.LeftHold);
+        this.blockGrid.placeAtIndex(76-18,this.LeftHold);
 
         this.RightHold2 = this.add.sprite(0,0,"RightHold");
         Align.scaleToGameW(this.RightHold2,0.05);
-        this.blockGrid.placeAtIndex(58,this.RightHold2);
+        this.blockGrid.placeAtIndex(139.2,this.RightHold2);
 
         this.RightHold = this.add.sprite(0,0,"RightHold");
         Align.scaleToGameW(this.RightHold,0.04);
-        this.blockGrid.placeAtIndex(58,this.RightHold);
+        this.blockGrid.placeAtIndex(76-24,this.RightHold);
 
         var frameNames = this.textures.get("bird").getFrameNames();
         console.log(frameNames);
