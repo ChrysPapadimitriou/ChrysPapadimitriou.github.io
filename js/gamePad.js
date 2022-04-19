e08f4a128b1f670d4cfd6d4c79d4074f7b93e648
Chryscy <chryspapacy@gmail.com>
2022-04-19 12:23:52 +0100

    v2

--- js/gamePad.js
+++ js/gamePad.js
@@ -6,15 +6,15 @@ class GamePad extends UIBlock
         this.emitter=EventDispatcher.getInstance();
 
         this.scene = config.scene;
-        this.blockGrid= config.blockGrid;
+        this.grid= config.grid;
 
         this.btnRight = this.scene.add.image(0,0,"Right");
         Align.scaleToGameW(this.btnRight,0.15);
-        this.blockGrid.placeAtIndex(452,this.btnRight);
+        this.grid.placeAtIndex(452,this.btnRight);
 
         this.bntLeft = this.scene.add.image(0,0,"Left");
         Align.scaleToGameW(this.bntLeft,0.15);
-        this.blockGrid.placeAtIndex(448,this.bntLeft);
+        this.grid.placeAtIndex(448,this.bntLeft);
 
         this.btnRight.setInteractive();
         this.bntLeft.setInteractive();
