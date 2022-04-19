b64d0ebc487e2a96819e03aa35dcfe94a2ad4574
Chryscy <chryspapacy@gmail.com>
2022-04-19 16:36:58 +0100

    v3

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -20,7 +20,7 @@ class SceneMain extends Phaser.Scene {
 
         this.emitter=EventDispatcher.getInstance();
         //add backgrounds 
-        let bg = this.add.image(0,0,"background").setOrigin(0,0);
+        let bg = this.add.image(0,0,"background");
         Align.scaleToGameW(bg,2);
         bg.displayHeight = this.sys.game.config.height;
         bg.displayWidth = this.sys.game.config.width*4;
