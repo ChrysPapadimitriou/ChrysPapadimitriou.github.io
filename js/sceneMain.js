a7b6c00aa76df3e0f364574b64268e676c456af5
Chryscy <chryspapacy@gmail.com>
2022-09-24 13:17:38 +0100

    v2

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -23,9 +23,9 @@ class SceneMain extends Phaser.Scene {
         //add backgrounds 
         let bg = this.add.image(0,0,"background");
         bg.displayHeight = this.sys.game.config.height;
-        bg.displayWidth = this.sys.game.config.width*4;
-        bg.y= game.config.height/2;
-        bg.x= game.config.width*3;
+        bg.displayWidth = this.sys.game.config.width;
+        bg.y= game.config.height;
+        bg.x= game.config.width;
 
         //set grid lines to locate positions
         this.blockGrid =new AlignGrid({
