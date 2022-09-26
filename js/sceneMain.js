6cde4a9cd7958d662c44db43e813a8726f0d6622
Chryscy <chryspapacy@gmail.com>
2022-09-26 13:20:43 +0100

    positions

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -45,23 +45,23 @@ class SceneMain extends Phaser.Scene {
 
         this.LeftHold = this.add.sprite(0,0,"LeftHold");
         Align.scaleToGameW(this.LeftHold,0.07);
-        this.blockGrid.placeAtIndex(595.4,this.LeftHold);
+        this.blockGrid.placeAtIndex(1700.15,this.LeftHold);
 //
         this.RightHold = this.add.sprite(0,0,"RightHold");
         Align.scaleToGameW(this.RightHold,0.07);
-        this.blockGrid.placeAtIndex(578,this.RightHold);
+        this.blockGrid.placeAtIndex(1026.8,this.RightHold);
 
         this.cloud = this.add.sprite(0,0,"cloud");
         Align.scaleToGameW(this.cloud,0.30);
-        this.blockGrid.placeAtIndex(500.3,this.cloud);
+        this.blockGrid.placeAtIndex(607.5,this.cloud);
 
         this.cloud2 = this.add.sprite(0,0,"cloud");
         Align.scaleToGameW(this.cloud2,0.30);
-        this.blockGrid.placeAtIndex(508.75,this.cloud2);
+        this.blockGrid.placeAtIndex(612.5,this.cloud2);
         
         this.cloud3 = this.add.sprite(0,0,"cloud");
         Align.scaleToGameW(this.cloud3,0.30);
-        this.blockGrid.placeAtIndex(516.6,this.cloud3);
+        this.blockGrid.placeAtIndex(617,this.cloud3);
         //testing anims
         var frameNames = this.textures.get("bird").getFrameNames();
         
